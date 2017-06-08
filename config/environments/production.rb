5f9ba4c7b4e10a4ef04755bdd148f20fa4d9f1c6
Nik <nerdvestor@gmail.com>
2017-06-07 18:06:27 -0700

    add devise user authenication before action and styling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "be_here_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'behere.herokuapp.com' }
+  config.action_mailer.perform_deliveries = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
